7094565d5e577059612871ba1aa20b2380ded909
Hus <email.of.huss@gmail.com>
2024-12-10 22:06:08 -0500

    pytest unconditional skip marker

--- pytest_practice/test_markers.py
+++ pytest_practice/test_markers.py
@@ -1,6 +1,13 @@
 import pytest
-
-@pytest.mark.basic_test
+π
+@pytest.mark.sanity_test
 def test_01():
     assert type(3.5) == float
 
+# Unconditional skip
+@pytest.mark.skip
+def test_02():
+    assert 1 == 1
+
+# @pytest.mark.skipif(condition=os)
+# def test_03():

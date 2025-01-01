5e2d2feb3aca6625c88a8f327ce10471595f1f74
Hus <email.of.huss@gmail.com>
2024-12-31 20:17:08 -0500

    Adding pytest parametrize for two params

--- pytest_practice/test_parametrized.py
+++ pytest_practice/test_parametrized.py
@@ -3,3 +3,9 @@ import pytest
 @pytest.mark.parametrize("input_expected", [25,30,50,70])
 def test_limit(input_expected):
     assert input_expected < 90
+
+@pytest.mark.parametrize("val1,val2", [(25,30),(50,70)])
+def test_two_params(val1, val2):
+    assert val1 < val2
+
+
